1701dd54cec32914edbc512bdac53eaf1219b310
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-27 23:19:01 +0530

    Add files via upload

---
+++ leastfrequentele.java
@@ -0,0 +1,21 @@
+
+// Least frequent element in an array
+// Input : arr[] = {1, 3, 2, 1, 2, 2, 3, 1}
+// Output : 3
+// Explanation: 3 appears minimum number of times in given array.
+
+
+// Input : arr[] = {10, 20, 30}
+// Output : 10 or 20 or 30
+import java.util.*;
+import java.io.*;
+
+public class leastfrequentele 
+{
+  
+  public static void main(String args[])
+  {
+    int arr[]={1,3,2,1,2,2,3,1};
+    System.out.print(leastfrequent(arr));
+  }    
+}
