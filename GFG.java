5ba49c50df8d6cb946ca3d83bf2f9fc543cbc984
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-27 23:18:40 +0530

    Add files via upload

---
+++ GFG.java
@@ -0,0 +1,82 @@
+
+//Last duplicate element in a sorted array
+
+// Input : arr[] = {1, 5, 5, 6, 6, 7}
+// Output :
+// Last index: 4
+// Last duplicate item: 6
+
+// Input : arr[] = {1, 2, 3, 4, 5}
+// Output : No duplicate found
+// import java.util.*;
+// public class duplicate 
+// {
+//     public static void duplastidx(int arr[],int n)
+//     {
+//         //if array is null or sixe is less
+//         if(arr==null || n<=0)
+//         {
+//             //compare elements
+        
+//             for(int i=n-1; i>0; i--)
+//             {
+//                 if(arr[i]==arr[i-1])
+//                 {
+//                     System.out.println("last index:+i");
+//                     System.out.println("last duplicate item:"+arr[i]);
+//                     return;
+//                 }
+//             }
+//             //if we reach here, the no duplicate found
+//             System.out.print("no duplicate found");
+//         }
+//     }
+//     public static void main(String args[])
+//     {
+//        int arr[]={1,5,5,6,6,7,9};
+//        int n=arr.length;
+//        duplastidx(arr, n);
+//     }
+// }
+// Java code to print last duplicate element 
+// and its index in a sorted array
+// import java.io.*;
+
+// class GFG
+// {
+// 	static void dupLastIndex(int arr[], int n) 
+// 	{
+// 		// if array is null or size is less 
+// 		// than equal to 0 return
+// 		if (arr == null || n <= 0) 
+// 			return;
+		
+// 		// compare elements and return last
+// 		// duplicate and its index
+// 		for (int i = n - 1; i > 0; i--) 
+// 		{
+// 			if (arr[i] == arr[i - 1]) 
+// 			{
+// 			System.out.println("Last index:" + i);
+// 			System.out.println("Last duplicate item: "
+// 							+ arr[i]);
+// 			return;
+// 			}
+// 		}
+		
+// 		// If we reach here, then no duplicate
+// 		// found.
+// 		System.out.print("no duplicate found");
+// 	}
+
+// 	// Driver code 
+// 	public static void main (String[] args) 
+// 	{
+// 		int arr[] = {1, 5, 5, 6, 6, 7, 9};
+// 		int n = arr.length;
+// 		dupLastIndex(arr, n);
+		
+// 	}
+// }
+
+// // This code is contributed by vt_m
