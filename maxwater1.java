16f83beffe07839ee3b9e586ecbab3261809af6e
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-29 23:04:34 +0530

    Add files via upload

---
+++ maxwater1.java
@@ -0,0 +1,50 @@
+
+//container with  most water- 2pointer approach i.e left and right pointer
+//height=1,8,6,2,5,4,8,3,7
+//TC=O(n) linear time complexity
+import java.util.*;
+public class maxwater1 
+{
+    public static int storewater(ArrayList<Integer> height)
+    {
+        int maxwater=0;
+        int lp=0;
+        int rp=height.size()-1;
+
+        while(lp < rp)
+        {
+            //calculate water area
+            int ht=Math.min(height.get(lp), height.get(rp));
+            int width=rp-lp;
+            int currwater=ht * width;
+            maxwater=Math.max(maxwater,currwater);
+
+            //update ptr
+            if(height.get(lp)<height.get(rp))
+            {
+                lp++;
+            }
+            else
+            {
+                rp--;
+            }
+        }
+        return maxwater;
+    }
+   public static void main(String args[])
+   {
+     ArrayList<Integer> height=new ArrayList<>();
+
+     height.add(1);
+     height.add(8);
+     height.add(6);
+     height.add(2);
+     height.add(5);
+     height.add(4);
+     height.add(8);
+     height.add(3);
+     height.add(7);
+
+     System.out.println(storewater(height));
+   }    
+}
